01e90ff717ae9a95c3f1de3ebb959551d9f4d888
Alex Harper <alex.harper@tactis.uk>
2025-03-25 11:09:54 +0000

    2.9.1

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 --tag tactis/caddy-cloudflaredns:2.8.4 --tag tactis/caddy-cloudflaredns:latest --load .
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 --tag tactis/caddy-cloudflaredns:2.9.1 --tag tactis/caddy-cloudflaredns:latest --push .
